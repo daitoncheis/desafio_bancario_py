d3bccbeafa0033d1834aa5459460d755ebc4c9f1
daitoncheis <daitoncheis@gmail.com>
2024-10-23 11:47:06 -0300

    Update projeto_v1.py

--- projeto_v1.py
+++ projeto_v1.py
@@ -9,12 +9,11 @@ menu = """
 
 saldo = 0
 limite = 500
-extrato = ""
+extrato = [] 
 numero_saques = 0
 LIMITE_SAQUES = 3
 
 while True:
-    
     opcao = input(menu)
     
     if opcao == "1":
@@ -22,8 +21,10 @@ while True:
         
         if valor_deposito > 0:
             saldo += valor_deposito
-            extrato += f"Depósito: R$ {valor_deposito:.2f}\n"
+            extrato_aux = extrato
+            extrato_aux.append(f"Depósito: R$ {valor_deposito:.2f}")
             print(f"Depósito de R$ {valor_deposito:.2f} realizado com sucesso!")
+
         else:
             print("Valor de depósito deve ser positivo.")
 
@@ -31,29 +32,38 @@ while True:
         valor_saque = float(input("Informe o valor do saque: "))
         
         if numero_saques < LIMITE_SAQUES:
+
             if valor_saque <= saldo and valor_saque <= limite:
                 saldo -= valor_saque
-                extrato += f"Saque: R$ {valor_saque:.2f}\n"
-                numero_saques += 1
+                extrato.append(f"Saque: R$ {valor_saque:.2f}")
                 print(f"Saque de R$ {valor_saque:.2f} realizado com sucesso!")
+
             elif valor_saque > saldo:
                 print("Saldo insuficiente para realizar o saque.")
+
             elif valor_saque > limite:
                 print(f"O valor do saque não pode ultrapassar R$ {limite:.2f}.")
+
         else:
             print("Número máximo de saques atingido.")
 
-
     elif opcao == "3":
-        print("Extrato:")
+        print("\n========= EXTRATO =========")
+
         if extrato:
-            print(extrato)
+
+            for movimento in extrato:
+
+                print(movimento)  # Exibe cada movimentação no extrato
         else:
             print("Nenhuma movimentação realizada.")
-        print(f"Saldo atual: R$ {saldo:.2f}") 
-    
+        print("==========================")
+        print(f"Saldo atual: R$ {saldo:.2f}")
+        print("==========================\n")
+
     elif opcao == "0":
+        print("Saindo...")
         break
 
     else:
-        print("Opção inválida, selecione novamente a operação desejada.")       
+        print("Opção inválida, selecione novamente uma opção no menu.")
